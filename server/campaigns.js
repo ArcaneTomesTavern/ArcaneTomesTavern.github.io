5cc17c6c6ac30e874a60bc0c3e797fe5b0dad89a
matteo404 <matteo.piras.3@studenti.unipd.it>
2023-09-22 12:22:06 +0200

    pagina campagna

--- server/campaigns.js
+++ server/campaigns.js
@@ -6,7 +6,13 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Inizializza Firebase con la tua configurazione
     const firebaseConfig = {
-        // La tua configurazione Firebase
+        apiKey: "AIzaSyCEc_jt85Nr4KS-4wXSPfAscmc0Vt0o4BM",
+        authDomain: "arcanetomestavern.firebaseapp.com",
+        projectId: "arcanetomestavern",
+        storageBucket: "arcanetomestavern.appspot.com",
+        messagingSenderId: "1035963633765",
+        appId: "1:1035963633765:web:7c1524769bc1e387ba768c",
+        measurementId: "G-Q2BES5BYC2"
     };
 
     firebase.initializeApp(firebaseConfig);
