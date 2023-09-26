49a1fe0d65fe978bfac3e348d729d7b20e073b66
Teorx78 <121100408+Teorx78@users.noreply.github.com>
2023-09-26 20:38:56 +0200

    Aggiornamento file js/html/css printPlayer

--- server/printPlayer.js
+++ server/printPlayer.js
@@ -19,6 +19,12 @@ const playerId = "U50R9nXSG7JV95b2xyMN"; // Sostituisci con l'ID del documento c
 const nome_pgDIV = document.getElementById("nome_pg");
 const datiDiv = document.getElementById("dati_class_rank"); // Assicurati di avere un elemento HTML con questo ID
 const experience = document.getElementById("exp");
+const forzaDIV = document.getElementById("forza");
+const destrezzaDIV = document.getElementById("destrezza");
+const costituzioneDIV = document.getElementById("costituzione");
+const intelligenzaDIV = document.getElementById("intelligenza");
+const saggezzaDIV = document.getElementById("saggezza");
+const carismaDIV = document.getElementById("carisma");
 
 function ottieniEVisualizzaDati() {
     playersRef.doc(playerId).get().then((doc) => {
@@ -32,22 +38,31 @@ function ottieniEVisualizzaDati() {
             const livello = data.livello;
             const esperienza = data.esperienza;
             const imageUrl = data.image_url;
+            const forza = data.forza;
+            const destrezza = data.destrezza;
+            const costituzione = data.costituzione;
+            const intelligenza = data.intelligenza;
+            const saggezza = data.saggezza;
+            const carisma = data.carisma;
             
 
             // Aggiungi i dati all'elemento HTML
             datiDiv.innerHTML = `<p>${razza} &nbsp ${classe} <br><br> <p>Livello ${livello}<\p>`;
 
-            nome_pgDIV.innerHTML = `
-                <p> ${nome_personaggio}</p>
-            `;
+            nome_pgDIV.innerHTML = `<p> ${nome_personaggio}</p>`;
 
-            experience.innerHTML = ` <br><p>${esperienza}/${level_exp[livello]}</p>
-            `;
+            experience.innerHTML = ` <br><p>${esperienza}/${level_exp[livello]}</p>`;
+            experience.style.width = `${(esperienza/level_exp[livello])*100}%`; 
 
             const immagineElement = document.getElementById('immagineFirebase');
             immagineElement.src = imageUrl;  
 
-            experience.style.width = `${(esperienza/level_exp[livello])*100}%`; //To do capire funzioanmento di ma esxp per livello in modo che sia graudale
+            ottieniSegno(forzaDIV, forza);
+            ottieniSegno(destrezzaDIV, destrezza);       
+            ottieniSegno(costituzioneDIV, costituzione);
+            ottieniSegno(intelligenzaDIV, intelligenza);
+            ottieniSegno(saggezzaDIV, saggezza);     
+            ottieniSegno(carismaDIV, carisma);
 
         } else {
             datiDiv.innerHTML = "<p>Il documento non esiste.</p>";
@@ -63,3 +78,10 @@ ottieniEVisualizzaDati();
 
 const level_exp = [1, 300, 900, 2700, 6500, 14000, 23000, 34000, 48000, 64000, 85000, 100000,
      120000, 140000, 165000, 195000, 225000, 265000, 305000, 355000];
+
+function ottieniSegno(elementoDiv, elementoData) {
+    if(elementoData > 0)
+        elementoDiv.innerHTML = `<p> +${elementoData}</p>`
+    else 
+        elementoDiv.innerHTML = `<p> ${elementoData}</p>`
+}
