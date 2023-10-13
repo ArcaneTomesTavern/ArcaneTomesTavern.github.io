b4165a9a8f22cf458eef3d04d95a276dff49ce63
matteo404 <matteo.piras.3@studenti.unipd.it>
2023-10-13 16:54:02 +0200

    pagina creazione mostro

--- server/createMonster.js
+++ server/createMonster.js
@@ -4,16 +4,23 @@ document.addEventListener("DOMContentLoaded", () => {
     var monster_type = document.getElementById("monster-type");
     var monster_subtype = document.getElementById("monster-subtype");
     var monster_size = document.getElementById("monster-size");
+    var monster_alignment = document.getElementById("alignment");
+    var monster_challenge = document.getElementById("challenge");
 
     //?oggetti file json
-    const jsonReader = new ReadJson("/server/resources/json/monster.json");       //? bisogna mettere il percorso assoluto non in base a dove si trova il file
+    const jsonMonsterReader = new ReadJson(
+        "/server/resources/json/monster.json"
+    ); //? bisogna mettere il percorso assoluto non in base a dove si trova il file
+    const jsonConstReader = new ReadJson(
+        "/server/resources/json/constants.json"
+    ); //? bisogna mettere il percorso assoluto non in base a dove si trova il file
 
     //?lettura dal file json
-    jsonReader.readData().then(() => {
+    jsonMonsterReader.readData().then(() => {
         //* lettura tipi mostri
-        const monsterTypes = jsonReader.getMonsterTypes();
+        const monsterTypes = jsonMonsterReader.getMonsterTypes();
         //console.log("Monster Types:", monsterTypes);
-        for(let i=0; i < monsterTypes.length; i++){
+        for (let i = 0; i < monsterTypes.length; i++) {
             const opt = document.createElement("option");
             opt.text = monsterTypes[i];
             opt.id = monsterTypes[i].replace(/ /g, "_");
@@ -22,123 +29,55 @@ document.addEventListener("DOMContentLoaded", () => {
         }
 
         //*lettura sotto tipi mostri
-        const monsteSubType = jsonReader.getMonsterSubType();
-        for(let i=0; i < monsteSubType.length; i++){
+        const monsteSubType = jsonMonsterReader.getMonsterSubType();
+        for (let i = 0; i < monsteSubType.length; i++) {
             const opt = document.createElement("option");
             opt.text = monsteSubType[i];
             opt.id = monsteSubType[i].replace(/ /g, "_");
             opt.tagName = monsteSubType[i].replace(/ /g, "_");
             monster_subtype.appendChild(opt);
         }
-        
+    });
+
+    jsonConstReader.readData().then(() => {
         //* lettura grandezza mostri
-        const monsterSize = jsonReader.getSize();
-        for(let i=0; i < monsterSize.length; i++){
+        const monsterSize = jsonConstReader.getSize();
+        for (let i = 0; i < monsterSize.length; i++) {
             const opt = document.createElement("option");
             opt.text = monsterSize[i];
             opt.id = monsterSize[i].replace(/ /g, "_");
             opt.tagName = monsterSize[i].replace(/ /g, "_");
             monster_size.appendChild(opt);
         }
+
+        //* lettura allineamento mostri
+        const monsterAlignment = jsonConstReader.getSize();
+        for (let i = 0; i < monsterAlignment.length; i++) {
+            const opt = document.createElement("option");
+            opt.text = monsterAlignment[i];
+            opt.id = monsterAlignment[i].replace(/ /g, "_");
+            opt.tagName = monsterAlignment[i].replace(/ /g, "_");
+            monster_alignment.appendChild(opt);
+        }
     });
+    
+    //*challenge rating del mostro
+    const challengeRate = ["0", "1/8", "1/4", "1/2"];
+    const TOT_CH = 30;
+    for (let i = 0; i < challengeRate.length; i++) {
+        const opt = document.createElement("option");
+        opt.text = challengeRate[i];
+        //opt.id = monsterChRate[i].replace(/ /g, "_");
+        opt.id = "ch_" + challengeRate[i];
+        opt.tagName = "ch_" + challengeRate[i];
+        monster_challenge.appendChild(opt);
+    }
+    for(let i = 1; i <= TOT_CH; i++){
+        const opt = document.createElement("option");
+        opt.text = i;
+        opt.id = "ch_" + i;
+        opt.tagName = "ch_" + i;
+        monster_challenge.appendChild(opt);
+    }
+    
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
